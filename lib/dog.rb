e42619d6f4403cdd11be13f6279e9d561525bd2c
Susan Meyers <smmeyers24@gmail.com>
2018-07-20 20:40:01 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def initialize(name, breed)
+    @name= name
+    @breed= Breed
+  end
+
+  def name= (name)
+    @name= name
+  end
+
+  def name
+    @name
+  end
+end
+
+ fido = Dog.new
